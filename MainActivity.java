db6292c76fd3ff1c57a63508ba751920e2a36e59
luigi2030 <luis-el-autentico2011@hotmail.com>
2026-08-25 12:44:57 -0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.lm.reseller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override public void onCreate(Bundle b) {
+        super.onCreate(b);
+        WebView w = new WebView(this);
+        w.setWebViewClient(new WebViewClient());
+        WebSettings s = w.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        w.loadUrl("file:///android_asset/index.html");
+        setContentView(w);
+    }
+    @Override public void onBackPressed() {
+        WebView w = (WebView) findViewById(android.R.id.content);
+        super.onBackPressed();
+    }
+}
